9f0a41ed49fdb6bbfe92c1c751d268a05e3c9bf8
Kalaiyarasi <askalalai31@gmailcom>
2018-09-05 22:36:04 +0530

    Add initial implementation for SNMP Inbound
    Minor changes

---
+++ src/main/java/org/wso2/carbon/inbound/snmp/SNMPConstants.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.inbound.snmp;
+
+/**
+ * This class will hold constants related to SNMP Inbound implementation.
+ */
+public class SNMPConstants {
+    public static final String HOST = "host";
+    public static final String PORT = "port";
+    public static final String IS_TCP = "isTCP";
+    public static final String COMBINER = "/";
+    public static final String CONTENT_TYPE = "text/plain";
+    public static final String SNMP_VERSION = "snmpVersion";
+    public static final String SNMP_VERSION2C = "2c";
+    public static final String SNMP_VERSION1 = "1";
+}
